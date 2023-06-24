0cbdd6ca5d9114e692391cd359af1d6753c4b6c6
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-25 08:02:32 +0900

    feat: userEvent - popover

--- 04/src/pages/summary/test/SummaryForm.test.js
+++ 04/src/pages/summary/test/SummaryForm.test.js
@@ -1,4 +1,5 @@
 import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
 test('initial conditions', () => {
@@ -12,16 +13,39 @@ test('initial conditions', () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test('Checkbox disabled button on first click and enables on second click', () => {
+test('Checkbox disabled button on first click and enables on second click', async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
   const checkbox = screen.getByRole('checkbox', {
     name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole('button', { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test('popover responds to hover', async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  // 처음에는 hidden
+  const nullPopover = screen.quferyByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // mouseover하면 appears
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // mouse out 하면 disappears
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
