543c40d51a2012ee68041bd4599453dcbe4a6208
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-24 11:51:41 +0900

    feat: change color button

---
+++ 2-change-color-button/src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('button has correct initial color', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+});
+
+test('button turns blue when clicked', () => {});
---
+++ 2-change-color-button/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <button style={{ backgroundColor: 'red' }}>Change to blue</button>
+    </div>
+  );
+}
+
+export default App;
